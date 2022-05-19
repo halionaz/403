de37d49c521d0718ae3c006c99867fcd7712fb75
halionaz <samhsj@naver.com>
2022-05-19 06:53:45 -0400

    React - express 연동

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(express.static(path.join(__dirname, "403-app/build")));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/403-app/build/index.html"));
 });
 
 app.listen(port, () => {
