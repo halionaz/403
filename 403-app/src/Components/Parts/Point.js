ddc6ff6ea37ff4cd8dcc20356f49bb73e07ce38f
halionaz <samhsj@naver.com>
2022-05-31 07:31:03 -0400

    Point Modal Styling

--- 403-app/src/Components/Parts/Point.js
+++ 403-app/src/Components/Parts/Point.js
@@ -9,7 +9,9 @@ export default function Point({ isOn, data, usedPoint }) {
         if(data !== null){
             let sum = 0;
             for (let i = 0; i < data.length; i++) {
-                sum += data[i].point;
+                if(data[i].issued){
+                    sum += data[i].point;
+                }
             }
             setTotal(sum);
         }
@@ -30,9 +32,19 @@ export default function Point({ isOn, data, usedPoint }) {
             <div className={style.history}>
                 <h3 className={style.historyTitle}>내역</h3>
                 <ul className={style.ul}>
-                    {data?.map((event, ind) => {
+                    {data?.filter(event => event.issued).map((event, ind) => {
                         return (
-                            <li className={style.li} key={ind}>
+                            <li className={style.li} style={{"background" : "rgba(46, 170, 220, 0.1)"}} key={ind}>
+                                {event.title} :: {event.point}점
+                            </li>
+                        );
+                    })}
+                </ul>
+                <h3 className={style.historyTitle}>받을 가점</h3>
+                <ul className={style.ul}>
+                    {data?.filter(event => !event.issued).map((event, ind) => {
+                        return (
+                            <li className={style.li} style={{"backgroundColor" : "rgb(235, 87, 87, 0.1)"}} key={ind}>
                                 {event.title} :: {event.point}점
                             </li>
                         );
