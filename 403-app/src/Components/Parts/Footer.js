ebd8be92570920f5b63827718743d2a82ed0b0ab
halionaz <samhsj@naver.com>
2022-06-03 06:52:37 -0400

    footer Components Added

--- 403-app/src/Components/Parts/Footer.js
+++ 403-app/src/Components/Parts/Footer.js
@@ -1,5 +1,9 @@
 import style from "../style/Footer.module.css";
 
 export default function Footer({today, highlight}){
-    return <div className={style.Footer}></div>
+    return <div className={style.Footer}>
+        <div className={style.dDay}></div>
+        <div className={style.percentage}></div>
+        <div className={style.etc}></div>
+    </div>
 }
