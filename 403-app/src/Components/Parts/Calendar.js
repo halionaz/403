f4fe4bb6a182aaab8f63a561e897e8f405fde6c6
halionaz <samhsj@naver.com>
2022-06-05 02:29:32 -0400

    요일 순서 변경, 스타일링

--- 403-app/src/Components/Parts/Calendar.js
+++ 403-app/src/Components/Parts/Calendar.js
@@ -4,13 +4,13 @@ const Calendar = () => {
     return (
         <div className={style.Calendar}>
             <div className={style.days}>
+                <div className={style.day}>SUN</div>
                 <div className={style.day}>MON</div>
                 <div className={style.day}>TUE</div>
                 <div className={style.day}>WED</div>
                 <div className={style.day}>THU</div>
                 <div className={style.day}>FRI</div>
                 <div className={style.day}>SAT</div>
-                <div className={style.day}>SUN</div>
             </div>
             <div className={style.dates}></div>
         </div>
