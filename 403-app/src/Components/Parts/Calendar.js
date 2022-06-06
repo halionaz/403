2528e8b850152eb65cb8df250372cdd8745c579a
halionaz <samhsj@naver.com>
2022-06-06 06:26:32 -0400

    ✨ Implement calendar

--- 403-app/src/Components/Parts/Calendar.js
+++ 403-app/src/Components/Parts/Calendar.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import style from "../style/Calendar.module.css";
+import Day from "./Day";
 
 const Calendar = ({ highlight, setHighlight }) => {
-
     const [currentYear, setCurrentYear] = useState(0);
     const [currentMonth, setCurrentMonth] = useState(0);
     // const [currentDate, setCurrentDate] = useState(0);
 
-    useEffect(()=>{
+    useEffect(() => {
         setCurrentYear(highlight.getFullYear());
         setCurrentMonth(highlight.getMonth());
         // setCurrentDate(highlight.getDate());
@@ -21,7 +21,11 @@ const Calendar = ({ highlight, setHighlight }) => {
     const [nextDate, setNextDate] = useState(0);
     const [nextDay, setNextDay] = useState(0);
 
-    useEffect(()=>{
+    const [prevCal, setPrevCal] = useState([]);
+    const [curCal, setCurCal] = useState([]);
+    const [nextCal, setNextCal] = useState([]);
+
+    useEffect(() => {
         // 이전 달의 마지막 날 날짜와 요일 구하기
         setStartDay(new Date(currentYear, currentMonth, 0));
         // 이번 달의 마지막날 날짜와 요일 구하기
@@ -33,10 +37,38 @@ const Calendar = ({ highlight, setHighlight }) => {
         setPrevDay(startDay?.getDay());
     }, [startDay]);
 
-    useEffect(()=>{
+    useEffect(() => {
         setNextDate(endDay?.getDate());
         setNextDay(endDay?.getDay());
     }, [endDay]);
+
+    useEffect(() => {
+        // 지난달 만들기
+        setPrevCal([]);
+        for (let i = prevDate - prevDay; i <= prevDate; i++) {
+            setPrevCal((prev) => {
+                return [...prev, i];
+            });
+        }
+    }, [prevDate, prevDay]);
+    useEffect(() => {
+        // 이번달 만들기
+        setCurCal([]);
+        for (let i = 1; i <= nextDate; i++) {
+            setCurCal((prev) => {
+                return [...prev, i];
+            });
+        }
+    }, [nextDate]);
+    useEffect(() => {
+        // 다음달 만들기
+        setNextCal([]);
+        for (let i = 1; i < (7 - nextDay === 7 ? 0 : 7 - nextDay); i++) {
+            setNextCal((prev) => {
+                return [...prev, i];
+            });
+        }
+    }, [nextDay]);
 
     return (
         <div className={style.Calendar}>
@@ -49,7 +81,29 @@ const Calendar = ({ highlight, setHighlight }) => {
                 <div className={style.day}>FRI</div>
                 <div className={style.day}>SAT</div>
             </div>
-            <div className={style.dates}></div>
+            <div className={style.dates}>
+                {prevCal.map((date, ind) => {
+                    return (
+                        <div key={ind} className={style.day}>
+                            <Day num={date} type={"prev"}></Day>
+                        </div>
+                    );
+                })}
+                {curCal.map((date, ind) => {
+                    return (
+                        <div key={ind} className={style.day}>
+                            <Day num={date} type={"cur"}></Day>
+                        </div>
+                    );
+                })}
+                {nextCal.map((date, ind) => {
+                    return (
+                        <div key={ind} className={style.day}>
+                            <Day num={date} type={"next"}></Day>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
